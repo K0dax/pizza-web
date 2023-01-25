4302a978fa0d99388f894b9f9314f371b2826cb2
K0dax <yancarlosbrg@outlook.com>
2023-01-25 19:22:19 -0300

    feat: login Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,32 @@
 import Head from "next/head";
+import Image from "next/image";
+import styles from "../../styles/home.module.scss";
+
+import logoImg from "../../public/logo.svg";
+import { Input } from "../components/ui/Input/";
+import { Button } from "../components/ui/Button/";
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>Pizzaria - Faça seu login</title>
       </Head>
-      <div>
-        <h1>Pizzaria</h1>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="Logo da Pizzaria" />
+        <div className={styles.login}>
+          <form>
+            <Input type="text" placeholder="Digite seu email" />
+
+            <Input type="password" placeholder="Digite sua senha" />
+
+            <Button type="submit" loading={false}>
+              Acessar
+            </Button>
+          </form>
+
+          <a className={styles.text}>Não possui uma conta? Cadastre-se</a>
+        </div>
       </div>
     </>
   );
